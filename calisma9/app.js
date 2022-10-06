e83be97626f34665a4b1efabc62e2793caf13c1c
Nurcan Şensoy <nurcansensoyy@gmail.com>
2022-10-06 16:51:09 +0300

    v-for

---
+++ calisma9/app.js
@@ -0,0 +1,12 @@
+new Vue({
+    el : "#app",
+    data : {
+      products : ["Monitor","Klavye","Aski","Masa"],
+      persons : [
+        {name : "Gökhan", email : "gokhan@gkandemir.com"},
+        {name : "Felakettin ", email : "felo@gmkandemir.com"},
+        {name : "Defne", email : "defne@gkandemir.com"},
+        {name : "İflasettin", email : "ifoo@gmkandemir.com"},
+      ]
+    }
+});
