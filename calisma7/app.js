a7b148511140cc3d2e577ffe92eef82996e4ab02
Nurcan Şensoy <nurcansensoyy@gmail.com>
2022-10-06 14:32:26 +0300

    class olmadan dinamik olarak stil işlemleri

--- calisma7/app.js
+++ calisma7/app.js
@@ -3,13 +3,20 @@ new Vue({
     el : "#app",
     data : {
         attachedClass : false,
-        color : "green"
+        color : "green",
+        height : 100
     },
     computed :{
         divClass : function(){
             return{
                 yellow : this.attachedClass, //class yellow olunca false
                 blue : !this.attachedClass //class blue olunca true
+            }
+        },
+        customStyle : function(){
+            return {
+                backgroundColor : this.color,
+                height : this.height +"px"
             }
         }
     }
