6a2c9e90c436abbfd3bce9c1eabd273bb75e8924
Nurcan Şensoy <nurcansensoyy@gmail.com>
2022-10-06 11:56:15 +0300

    ödev3

---
+++ odev-3/app.js
@@ -0,0 +1,21 @@
+new Vue({
+        el: '#exercise',
+        data: {
+            value: 0,
+            timeout : 2000
+        },
+        computed : {
+            result : function (){
+                return this.value == 33 ? "Tamamlandı" : "Henüz Bitmedi!!";
+            }
+        },
+        watch : {
+            result : function(value){
+                vm = this;
+                setTimeout(function (){
+                    vm.value= 0;
+                },this.timeout);
+                
+            }
+        }
+    });
