d32d6e90e5332dd1393d8fc84693ee14233c9be6
Nurcan Şensoy <nurcansensoyy@gmail.com>
2022-10-07 14:14:56 +0300

    v-for

--- calisma9/app.js
+++ calisma9/app.js
@@ -3,10 +3,10 @@ new Vue({
     data : {
       products : ["Monitor","Klavye","Aski","Masa"],
       persons : [
-        {name : "Gökhan", email : "gokhan@gkandemir.com"},
+        {name : "Gokhan", email : "gokhan@gkandemir.com"},
         {name : "Felakettin ", email : "felo@gmkandemir.com"},
         {name : "Defne", email : "defne@gkandemir.com"},
-        {name : "İflasettin", email : "ifoo@gmkandemir.com"},
+        {name : "Iflasettin", email : "ifoo@gmkandemir.com"},
       ]
     }
 });
--- odev-4/app.js
+++ odev-4/app.js
@@ -1,11 +1,24 @@
 new Vue({
   el: '#exercise',
   data: {
+    
+    effectClass:false,
 
   },
   methods: {
     startEffect: function() {
-    
+    vm = this;
+    setInterval(function(){
+      vm.effectClass = !vm.effectClass;
+    },1000);
+    },
+  },
+  computed : {
+    updateEffectClass : function(){
+      return {
+        highlight:this.effectClass,
+        shrink : !this.effectClass
+      }
     }
   }
 });
