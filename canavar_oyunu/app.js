e045057ba3bab37182b94b801aa637f5df307a31
Nurcan Şensoy <nurcansensoyy@gmail.com>
2022-10-10 18:27:16 +0300

    kod optimizasyonu

--- canavar_oyunu/app.js
+++ canavar_oyunu/app.js
@@ -4,6 +4,17 @@ new Vue({
     player_heal : 100,
     monster_heal : 100,
     game_is_on : false,
+    attack_multiple :10,
+    special_attack_multiple : 25,
+    heal_up_multiple : 20,
+    monster_attack_multiple : 15,
+    log_text : {
+        attack : "OYUNCU ATAĞI :",
+        special_attack :"ÖZEL OYUNCU ATAĞI : ",
+        monster_attack : "CANAVAR ATAĞI :",
+        heal_up : "İLK YARDIM",
+        give_up : "OYUNCU PES ETTİ!!"
+    },
     logs : [],
    },
    methods : {
@@ -11,40 +22,40 @@ new Vue({
         this.game_is_on = true;
     },
     attack : function(){
-        var point = Math.ceil(Math.random()* 10); //math.ceil ile virgülsüz değer alırız 
+        var point = Math.ceil(Math.random()* this.attack_multiple); //math.ceil ile virgülsüz değer alırız 
         //this.monster_heal = this.monster_heal - point;
         this.monster_heal -= point;
 
-        this.add_to_log({turn : "p", text : "OYUNCU ATAĞI ("+ point +")"});
+        this.add_to_log({turn : "p", text : this.log_text.attack + point});
 
         this.monster_attack(); //oyuncu her saldırdığında monster da ona saldırmalı.
     },
     special_attack : function(){
-        var point = Math.ceil(Math.random()* 25);  // 25 yaptıkdaha güçlü saldırı olması için.
+        var point = Math.ceil(Math.random()* this.special_attack_multiple);  // 25 yaptık daha güçlü saldırı olması için.
         this.monster_heal -= point;
 
-        this.add_to_log({turn : "p", text : "ÖZEL OYUNCU ATAĞI ("+ point +")"});
+        this.add_to_log({turn : "p", text : this.log_text.special_attack + point});
 
         this.monster_attack();
     },
     heal_up : function(){ //İLK YARDIM
-        var point = Math.ceil(Math.random()* 20); //math.ceil ile virgülsüz değer alırız 
+        var point = Math.ceil(Math.random()* this.heal_up_multiple); //math.ceil ile virgülsüz değer alırız 
         //this.monster_heal = this.monster_heal - point;
         this.player_heal += point;
 
-        this.add_to_log({turn : "p", text : "İLK YARDIM ("+ point +")"});
+        this.add_to_log({turn : "p", text : this.log_text.heal_up + point});
 
         this.monster_attack();
     },
     give_up : function(){ //PES ET
         this.player_heal = 0;
-        this.add_to_log({turn : "p", text : "OYUNCU PES ETTİ!!"});
+        this.add_to_log({turn : "p", text : this.log_text.give_up});
     },
     monster_attack : function() {
-        var point = Math.ceil(Math.random()* 15); 
+        var point = Math.ceil(Math.random()* this.monster_attack_multiple); 
         this.player_heal -= point;
 
-        this.add_to_log({turn : "m" ,text : "CANAVAR ATAĞI ("+ point +")"});
+        this.add_to_log({turn : "m" ,text : this.log_text.monster_attack + point});
     },
     add_to_log : function(log){
         this.logs.push(log); //logs array inin içine log objelerini pushladık.
