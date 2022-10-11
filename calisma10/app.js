b12adfc39eaae2170c3e5dffa2ac2d9634b5344c
Nurcan Şensoy <nurcansensoyy@gmail.com>
2022-10-11 16:52:57 +0300

    local olarak component tanımlama

--- calisma10/app.js
+++ calisma10/app.js
@@ -1,15 +1,27 @@
-Vue.component('my-comp',{
+var cmp ={
     data : function(){
        return {
         status : "Pasif"
        }
     },
-
-    template : "<p> Sunucu Durumu : {{ status }} </p>"
-});
+ 
+    template : '<p> Sunucu Durumu : {{ status }} (<button @click = "changeStatus"> Değiştir</button>) </p>',
+    methods : {
+        changeStatus : function(){
+            this.status="Aktif";
+        }
+    }
+}
 
 
 
 new Vue({
     el : "#app",
+    components :{
+        "my-comp" : cmp
+    }
+});
+
+new Vue({
+    el : "#app2",
 });
