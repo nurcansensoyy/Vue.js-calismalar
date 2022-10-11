f8ed8510d8b37fa2132802c0c8b3e82628bdd611
Nurcan Şensoy <nurcansensoyy@gmail.com>
2022-10-11 16:08:48 +0300

    component tanımlama

---
+++ calisma10/app.js
@@ -0,0 +1,15 @@
+Vue.component('my-comp',{
+    data : function(){
+       return {
+        status : "Pasif"
+       }
+    },
+
+    template : "<p> Sunucu Durumu : {{ status }} </p>"
+});
+
+
+
+new Vue({
+    el : "#app",
+});
