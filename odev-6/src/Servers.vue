6914845ec8b7994a3553b8e84a456b1bfab74b71
Nurcan Şensoy <nurcansensoyy@gmail.com>
2022-10-13 10:39:39 +0300

    .

--- odev-6/src/Servers.vue
+++ odev-6/src/Servers.vue
@@ -3,7 +3,7 @@
         <ul class="list-group">
           <li
             class="list-group-item"
-            v-for="index in 5">
+            v-for="index in 5" :key="index">
             Sunucu #{{ index }} 
           </li>
         </ul>
