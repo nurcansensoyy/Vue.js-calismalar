c1bf775b06786485a8e02f600b401fb7b5d4af48
Nurcan Şensoy <nurcansensoyy@gmail.com>
2022-10-13 14:22:32 +0300

    .

--- bolum-8-componentler-arasi-iletisim-baslangic-kodu/src/components/UserDetail.vue
+++ bolum-8-componentler-arasi-iletisim-baslangic-kodu/src/components/UserDetail.vue
@@ -3,13 +3,28 @@
     <h3>Child Component 1</h3>
     <p>Ben User.vue isimli Parent Component'in içerisindeki bir Child componentim</p>
     <p>Kullanici adi: {{name}}</p>
+    <p>Kullanici adi: {{switchNAme()}}</p>
+    <button @click="sendToParent">Veriyi parent a gonder</button>
   </div>
 </template>
 
 
 <script>
 export default {
-  props : ["name"],
+  props : {
+    name : {
+      type : Object,
+      default :"videosinif.com"
+    }
+  },//chill parent ilişkisi
+  methods : {
+    switchNAme(){
+      return this.name.split("").reverse().join("");
+    },
+    sendToParent(){
+      this.$emit("data","NurcanSensoy");
+    }
+  }
 }
 </script>
 
