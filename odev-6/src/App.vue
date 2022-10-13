d24b9e70c1e927ed1e070197068da66fb89b15bf
Nurcan Şensoy <nurcansensoyy@gmail.com>
2022-10-13 10:38:00 +0300

    componentlerine ayırma

--- odev-6/src/App.vue
+++ odev-6/src/App.vue
@@ -1,39 +1,35 @@
 <template>
   <div class="container">
-    <div class="row">
-      <div class="col-xs-12">
-        <header>
-          <h1>Sunucu Durumu</h1>
-        </header>
-      </div>
-    </div>
+    
+    <app-header></app-header>
+    
+
     <hr>
     <div class="row">
-      <div class="col-xs-12 col-sm-6">
-        <ul class="list-group">
-          <li
-            class="list-group-item"
-            v-for="index in 5">
-            Sunucu #{{ index }} 
-          </li>
-        </ul>
-      </div>
-      <div class="col-xs-12 col-sm-6">
-        <p>Sunucu Bilgisi güncel değil!!</p>
-      </div>
+
+      <app-servers></app-servers>
+      <app-servers-details></app-servers-details>
+
+      
     </div>
     <hr>
-    <div class="row">
-      <div class="col-xs-12">
-        <footer>
-          <p>Tüm Hakları Saklıdır | videosinif.com</p>
-        </footer>
-      </div>
-    </div>
+    <app-footer></app-footer>
   </div>
 </template>
 
 <script>
+import Header from "./Header.vue";
+import Footer from "./Footer.vue";
+import Servers from "./Servers.vue";
+import ServerDetails from "./ServerDetails.vue";
+export default {
+  components :{
+    "app-header" : Header,
+    "app-footer" : Footer,
+    "app-servers" : Servers,
+    "app-servers-details" : ServerDetails
+  }
+}
 </script>
 
 <style>
