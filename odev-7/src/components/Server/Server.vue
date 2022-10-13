e624e4ef7a5f1dd8ad2d507abdc58f4fca1c56a0
Nurcan Şensoy <nurcansensoyy@gmail.com>
2022-10-13 16:30:02 +0300

    ödev7

---
+++ odev-7/src/components/Server/Server.vue
@@ -0,0 +1,14 @@
+<template>
+  <li
+        class="list-group-item">
+        Sunucu #{{ index }}
+      </li>
+</template>
+
+<script>
+
+</script>
+
+<style>
+
+</style>
---
+++ odev-7/src/components/Server/Servers.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="col-xs-12 col-sm-6">
+    <ul class="list-group">
+      <appServer v-for="server in servers" :key="server">{{server}}</appServer>
+    </ul>
+  </div>
+</template>
+
+<script>
+import Server from "./Server.vue";
+
+export default {
+  components :{
+    appServer : Server,
+  },
+data : function(){
+    return {
+        servers : [
+            {id : 1, status : "Kritik"},
+            {id : 2, status : "Normal"},
+            {id : 3, status : "Tehlikede"},
+            {id : 4, status : "Bilinmiyor"},
+        ]
+    }
+}
+}
+</script>
