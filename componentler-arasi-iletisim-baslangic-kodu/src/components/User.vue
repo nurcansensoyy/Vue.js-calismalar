d9ecdea04baa6f7cfbe7536b1ff846089052d43a
Nurcan Şensoy <nurcansensoyy@gmail.com>
2022-10-13 11:50:36 +0300

    componentler araı iletişim

---
+++ componentler-arasi-iletisim-baslangic-kodu/src/components/User.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="container">
+    <h1>Parent Component (User)</h1>
+    <p>Burası parent component yani herşeyin import edildiği component :)</p>
+    <button @click="changeName">Adimi Degistir</button>
+    <hr>
+    <div class="row">
+      <app-user-detail></app-user-detail>
+      <app-user-edit></app-user-edit>
+    </div>
+  </div>
+</template>
+<script>
+  import UserDetail from "./UserDetail";
+  import UserEdit from "./UserEdit";
+  export default {
+    components : {
+      appUserDetail : UserDetail,
+      appUserEdit : UserEdit,
+    },
+    data : function(){
+      title : "Nurcan"
+    },
+    methods : {
+      changeName(){
+        this.title = "Sensoy"
+      }
+    }
+  }
+</script>
+
+<style>
+  div.container{
+    margin-top: 30px;
+    padding: 20px 40px;
+    background-color: #6a8d99;
+    border: 1px solid #666;
+  }
+  div.row {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+</style>
