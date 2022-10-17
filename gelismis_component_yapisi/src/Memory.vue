1115bdb2553561895c00b6776541c742f414b2f1
Nurcan Şensoy <nurcansensoyy@gmail.com>
2022-10-17 14:42:02 +0300

    default slot ve slot default

--- gelismis_component_yapisi/src/Memory.vue
+++ gelismis_component_yapisi/src/Memory.vue
@@ -6,6 +6,14 @@
     </div>
     <div class="memory">
       <slot name = "content"></slot>
+      <!-- eger name eklenmemis bir slot varsa ona slot eklenmeyen bir slot u direkt alir buna default slot denir.-->
+    </div>
+    <div class="memory">
+      <slot name = "author">
+        <h1>Author</h1>
+        <p>Buraya herhangi bir gelmemis!!!</p>
+        <!-- eger slot name i ile işleşen bir slot bulamazsa içine kendimiz verileri yazabiliriz. Buna slot default denir.-->
+      </slot>
     </div>
    
   </div>
