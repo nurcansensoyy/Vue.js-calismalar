a6b0815e7c9e983ad86073b1d3c097d64b9f567b
Nurcan Şensoy <nurcansensoyy@gmail.com>
2022-10-17 15:32:06 +0300

    Dinamik componentler

--- gelismis_component_yapisi/src/App.vue
+++ gelismis_component_yapisi/src/App.vue
@@ -2,21 +2,42 @@
   <div class="container">
     <div class="row">
       <div class="col-md-6 col-md-offset-3">
-        <app-memory>
+
+        <button @click= "selectedComponent = 'appMemory'" class="btn-sm btn btn-primary">appMemory</button>
+        <button @click="selectedComponent = 'appNewMemory'" class="btn-sm btn btn-primary">appNewMemory</button>
+        <button @click= "selectedComponent = 'appAuthor'" class="btn-sm btn btn-primary">appAuthor</button>
+        <hr>
+        <p>{{selectedComponent}} </p>
+        <hr>
+        <component :is = "selectedComponent">
           <h1 slot = "heading">Guzel Bir Gun</h1>
           <p slot = "content">Bu cok guzel bir aniydi...</p>
-        </app-memory>
+        </component>
+<!-- selectedComponent app-memory gibi seçicidir-->
+        <!--<app-memory>
+          <h1 slot = "heading">Guzel Bir Gun</h1>
+          <p slot = "content">Bu cok guzel bir aniydi...</p>
+        </app-memory>-->
       </div>
     </div>
   </div>
 </template>
 
 <script>
-  import Memory from "./Memory";
+  import Memory from "./components/Memory";
+  import NewMemory from "./components/NewMemory.vue";
+  import Author from "./components/Author.vue";
 
   export default {
+    data : function(){
+      return { 
+        selectedComponent : "appMemory",
+      }
+    },
     components : {
-      appMemory : Memory
+      appMemory : Memory,
+      appNewMemory : NewMemory,
+      appAuthor : Author,
     }
   }
 </script>
---
+++ gelismis_component_yapisi/src/components/Author.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>Author Componenti</h1>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
+ 
