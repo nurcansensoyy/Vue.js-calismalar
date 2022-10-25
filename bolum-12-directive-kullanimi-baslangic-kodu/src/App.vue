93643353d4cf95d7334cd3b9a78f250a37df3e0d
Nurcan Şensoy <nurcansensoyy@gmail.com>
2022-10-25 15:53:57 +0300

    local olrak directive tanımlama

--- bolum-12-directive-kullanimi-baslangic-kodu/src/App.vue
+++ bolum-12-directive-kullanimi-baslangic-kodu/src/App.vue
@@ -24,7 +24,30 @@
 </template>
 
 <script>
-  export default {}
+export default {
+  directives: { //local olarak directive tanımlama
+    "color": {
+      bind(el, binding, vnode) {
+        if (binding.modifiers["delay"]) {
+          setTimeout(() => {
+            if (binding.arg == "background") { //eğer background varsa arka planın rengi değişecek yoksa yazının rengi kırmızı olacak
+              el.style.backgroundColor = binding.value; //binding.value ile string veriyi aldık
+            } else {
+              el.style.color = binding.value;
+            }
+          }, 2000);
+
+        } else {
+          if (binding.arg == "background") { //eğer background varsa arka planın rengi değişecek yoksa yazının rengi kırmızı olacak
+            el.style.backgroundColor = binding.value; //binding.value ile string veriyi aldık
+          } else {
+            el.style.color = binding.value;
+          }
+        }
+      }
+      }
+    }
+  }
 </script>
 
 <style>
