9187a179d1ce476e32292efb05a9a43a44b32a65
Nurcan Şensoy <nurcansensoyy@gmail.com>
2022-10-27 10:29:18 +0300

    slide işlemi

--- bolum-14-animation-ve-transition-baslangic-kodu/src/App.vue
+++ bolum-14-animation-ve-transition-baslangic-kodu/src/App.vue
@@ -6,11 +6,13 @@
         <hr>
         <button class="btn btn-primary" @click="show = !show">Kutuyu göster</button>
         <br><br>
-        
-        <transition>
-        
-          <div class="alert alert-success" v-if="show">Bu bir alert kutusudur.</div>
 
+        <transition name = "fade">
+          <div class="alert alert-success" v-if="show">Bu bir alert kutusudur.</div>
+        </transition>
+        <hr>
+        <transition name="slide">
+          <div class="alert alert-warning" v-if="show">Bu bir alert kutusudur.</div>
         </transition>
       </div>
     </div>
@@ -29,4 +31,52 @@ export default {
 </script>
 
 <style>
+.fade-enter{
+  opacity: 0;
+}
+.fade-enter-active{
+  transition: opacity 1s;
+}
+/* .fade-leave{
+  opacity: 1; //opacity zaten 1 ve gerek yok
+} */
+.fade-leave-active{
+  transition: opacity 1s;
+  opacity: 0;
+
+}
+
+.slide-enter{}
+
+.slide-enter-active{
+animation : slide-in 1s ease-out forwards ;
+}
+.slide-leave{
+
+}
+.slide-leave-active{
+animation: slide-out 1s ease-out forwards;
+}
+
+@keyframes slide-in {
+from{
+  transform: translateY(20px); /*20px aşağıdan yukarı gelmesi için */
+}
+to{
+  transform: translateY(0px);
+}
+  
+}
+
+@keyframes slide-out {
+  from {
+    transform: translateY(0px);
+   
+  }
+
+  to {
+    transform: translateY(20px);/*20px yukarıdan aşağı gelmesi için */
+  }
+
+}
 </style>
