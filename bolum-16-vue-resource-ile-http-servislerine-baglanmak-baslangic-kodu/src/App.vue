c76b12c14061cb098cb969a450f7f81712917034
Nurcan Şensoy <nurcansensoyy@gmail.com>
2022-11-01 10:46:33 +0300

    http sunucusu ile bağlama

--- bolum-16-vue-resource-ile-http-servislerine-baglanmak-baslangic-kodu/src/App.vue
+++ bolum-16-vue-resource-ile-http-servislerine-baglanmak-baslangic-kodu/src/App.vue
@@ -3,6 +3,10 @@
     <div class="row">
       <div class="col-md-6 col-md-offset-3">
         <h3>Vue-Resource</h3>
+        <div class="form-group">
+          <input type="text" class="form-control"  v-model="username">
+        </div>
+        <button class="btn btn-primary" @click = "saveUser">Kaydet</button>
       </div>
     </div>
   </div>
@@ -10,6 +14,21 @@
 
 <script>
 export default {
+  data() {
+    return {
+      username : null
+    }
+  },
+  methods: {
+    saveUser() {
+      
+      this.$http.post("https://vuejs-vue-resource-d030f-default-rtdb.firebaseio.com/users.json", { username: this.username })
+        .then((response) => {
+          console.log(response);
+        
+      })
+   }
+  }
 }
 </script>
 
--- bolum-16-vue-resource-ile-http-servislerine-baglanmak-baslangic-kodu/src/main.js
+++ bolum-16-vue-resource-ile-http-servislerine-baglanmak-baslangic-kodu/src/main.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
-import VueResource from "vue-"vue-resource
+import VueResource from "vue-resource";
+//Vue resource ile HTTP servislerine bağlanırız
+Vue.use(VueResource);
 
 new Vue({
   el: '#app',
