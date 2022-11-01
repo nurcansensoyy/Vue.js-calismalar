dca3220768a0bdfa85689c51e24970dae77e1758
Nurcan Şensoy <nurcansensoyy@gmail.com>
2022-11-01 16:56:05 +0300

    exact kullanımı

--- bolum-17-vue-router-ile-route-yonetimi-baslangic-kodu/src/components/Header.vue
+++ bolum-17-vue-router-ile-route-yonetimi-baslangic-kodu/src/components/Header.vue
@@ -3,11 +3,14 @@
     <router-link
     active-class="active" 
     to ="/"
+    exact
     tag = "li">
         <a>Home</a>
     </router-link> 
     <!--active-class home veya user a bastığımızda hangisinin seçili olduğunu belli etmek için kullanılır -->
-    
+
+    <!-- exact ile to daki tam tanım neyse oranın seçili olduğunu göster diyoruz -->
+
     <!-- Router-link ile diyorum ki to ile gitmesi gereken yeri belirtiyorum
     tag ile sonunda görünmesi gerekeni yani li olmasını istedik -->
 
