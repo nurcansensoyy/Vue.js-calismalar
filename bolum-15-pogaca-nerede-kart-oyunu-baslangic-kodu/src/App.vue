45cd434acb59116a24e897d215f41ca5f9618094
Nurcan Şensoy <nurcansensoyy@gmail.com>
2022-11-01 10:03:54 +0300

    Http servislerine bağlanma

--- bolum-15-pogaca-nerede-kart-oyunu-baslangic-kodu/src/App.vue
+++ bolum-15-pogaca-nerede-kart-oyunu-baslangic-kodu/src/App.vue
@@ -1,6 +1,26 @@
 <template>
+    <component
+        :is="activeComponent" 
+    ><!--activeComponent ile hangisini o an göstermek istediğimizi belirleriz-->
+
+    </component>
 </template>
 <script>
-export default {}
+import GameCards from "./components/GameCards.vue";
+import Failure from "./components/Failure.vue";
+import Celebrate from "./components/Celebrate.vue";
+
+export default {
+    components: {
+        appGameCards: GameCards,
+        appFailrue: Failure,
+        appCelebrate : Celebrate,
+    },
+    data() {
+        return {
+            activeComponent : "app-game-cards"
+        }
+    }
+}
 </script>
 <style></style>
