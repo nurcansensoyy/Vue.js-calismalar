86b68605f0f5432c93025523782e06754495a901
Nurcan Şensoy <nurcansensoyy@gmail.com>
2022-11-08 14:55:11 +0300

    beforeRouterLeave ile çıkış süreçlerini yönetmek

--- bolum-17-vue-router-ile-route-yonetimi-baslangic-kodu/src/components/user/UserEdit.vue
+++ bolum-17-vue-router-ile-route-yonetimi-baslangic-kodu/src/components/user/UserEdit.vue
@@ -4,10 +4,29 @@
     <hr>
     <p>Ad : {{$route.query.name}}</p>
     <p>Soyad : {{$route.query.lastName}}</p>
+    <button class="btn btn-primary" @click="saved=true">Onayla</button>
     <div style="height : 800px"></div>
     <p id="data">Burada bilgiler olabilir...</p>
   </div>
 </template>
 <script>
-  export default {}
+export default {
+  data() {
+    return {
+        saved : false,
+      }
+  },
+  beforeRouteLeave(to, from, next) {
+    if (this.saved) {
+      next();
+    } else {
+      if (confirm("Tüm bilgileriniz kaybolacak emin misiniz?")) {
+        next();
+      } else {
+        next(false)
+       }
+    }
+    
+    }
+  }
 </script>
