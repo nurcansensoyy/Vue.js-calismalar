afd1080140ed6682cfd2ba982b1e284afd23f7a3
Nurcan Şensoy <nurcansensoyy@gmail.com>
2022-11-08 15:04:23 +0300

    Route ları lazy load olaraka yüklemek

--- bolum-17-vue-router-ile-route-yonetimi-baslangic-kodu/src/routes.js
+++ bolum-17-vue-router-ile-route-yonetimi-baslangic-kodu/src/routes.js
@@ -1,9 +1,29 @@
 import Home from "./components/Home";
-import User from "./components/user/User";
-import UserStart from "./components/user/UserStart";
-import UserDetail from "./components/user/UserDetail";
-import UserEdit from "./components/user/UserEdit";
 import Header from "./components/Header";
+
+const User = resolve => {
+    require.ensure(["./components/user/User.vue"], () => {
+        resolve(require("./components/user/User.vue"));
+    } )
+}
+
+const UserStart = resolve => {
+    require.ensure(["./components/user/UserStart.vue"], () => {
+        resolve(require("./components/user/UserStart.vue"));
+    } )
+}
+
+const UserDetail = resolve => {
+    require.ensure(["./components/user/UserDetail.vue"], () => {
+        resolve(require("./components/user/UserDetail.vue"));
+    } )
+}
+
+const UserEdit = resolve => {
+    require.ensure(["./components/user/UserEdit.vue"], () => {
+        resolve(require("./components/user/UserEdit.vue"));
+    } )
+}
 
 
 export const routes = [
