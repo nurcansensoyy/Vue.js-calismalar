e7016b47089e08851e09d9cb237c8c4e1b33b02c
Nurcan Şensoy <nurcansensoyy@gmail.com>
2022-11-09 10:36:21 +0300

    getters kullanımı

---
+++ bolum-18-vuex-baslangic-kodu/src/components/Counter.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <button class="btn btn-success" @click="incrementCounter">+ Arttır</button>
+    <button class="btn btn-danger" @click="decrementCounter">- Azalt</button>
+  </div>
+</template>
+
+<script>
+export default {
+  methods : {
+    incrementCounter(){
+      //this.$emit("counterEvent", 1);
+      this.$store.state.counter++;
+    },
+    decrementCounter(){
+     // this.$emit("counterEvent", -1);
+      this.$store.state.counter--;
+    }
+  }
+}
+</script>
+
+<style>
+</style>
