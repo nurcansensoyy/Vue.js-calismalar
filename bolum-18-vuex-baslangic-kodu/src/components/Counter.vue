afdfc205df4691a13c8a865f6c26b6748699dce1
Nurcan Şensoy <nurcansensoyy@gmail.com>
2022-11-09 13:35:02 +0300

    parametre işlemleri

--- bolum-18-vuex-baslangic-kodu/src/components/Counter.vue
+++ bolum-18-vuex-baslangic-kodu/src/components/Counter.vue
@@ -2,22 +2,43 @@
   <div>
     <button class="btn btn-success" @click="increaseCounter">+ Arttır</button>
     <button class="btn btn-danger" @click="decreaseCounter">- Azalt</button>
+    <hr>
+    <button class="btn btn-success" @click="increment">+ Arttır</button>
+    <button class="btn btn-danger" @click="decrement">- Azalt</button>
+<hr>
+<button class="btn btn-success" @click="incAsync({time : 1000})">+ Async Arttır</button>
+
+
   </div>
 </template>
 
 <script>
-import { mapMutations } from 'vuex';
+import { mapMutations, mapActions } from 'vuex';
 export default {
   methods: {
     ...mapMutations([
       "increaseCounter",
       "decreaseCounter"
-    ])
-    
+    ]),
+    ...mapActions([
+      "increment",
+      "decrement",
+      "incAsync"
+    ]),
+    // increment() {
+    //   this.$store.dispatch("increment")//actions lar dispatch ile çalıştırılır.
+    // },
+    // decrement() {
+    //   this.$store.dispatch("decrement")
+    // },
+    // incrementAsync() {
+    //   this.$store.dispatch("incAsync")//actions lar dispatch ile çalıştırılır.
+    // },
+   
     // incrementCounter(){
     //   //this.$emit("counterEvent", 1);
     //   //this.$store.state.counter++;
-    //   this.$store.commit("increaseCounter");
+    //   this.$store.commit("increaseCounter");//mutations çalıştırmak için commit kullanılır.
     // },
     // decrementCounter(){
     //  // this.$emit("counterEvent", -1);
--- bolum-18-vuex-baslangic-kodu/src/store/store.js
+++ bolum-18-vuex-baslangic-kodu/src/store/store.js
@@ -5,7 +5,8 @@ Vue.use(Vuex);
 
 export const store = new Vuex.Store({
     state: {
-        counter : 0
+        counter: 0,
+        value : 0,
     },
     getters: {
         getDoubleCounter(state) {
@@ -13,14 +14,34 @@ export const store = new Vuex.Store({
         },
         stringCounter(state) {
             return state.counter + ".kez tıklandı."
+        },
+        getValue(state) {
+            return state.value;
         }
     },
     mutations: { //state in içerisinde bulunan verilerin değerini güncellemek için kullanılan fonksiyonlardan biridir.
-        increaseCounter(state) {
-            state.counter++;  //state in içindeki counter değerine value ile dışardan gelen value değerini ekle.
+        increaseCounter(state, payload) {
+            state.counter += payload;  //state in içindeki counter değerine value ile dışardan gelen value değerini ekle.
         },
         decreaseCounter(state) {
             state.counter--;
+        },
+        setValue(state, payload) {
+            state.value = payload;
         }
+    },
+    actions: { //asenkron bir şekilde çalışan istek göndermek gibi olaylarda mutations ile child arasına girer.
+        increment({commit}) {//commit vasıtasıyla mutations içindeki fonksiyonlara erişebiliriz.
+            commit("increaseCounter");
+        },
+        decrement({commit}) {
+            commit("decreaseCounter");
+        },
+        incAsync({ commit }, payload) { //payload parametresidir
+            setTimeout(() => {
+                commit("increaseCounter", 1 ) //1 arttır
+            }, payload.time)
+        },
+       
     }
 });
