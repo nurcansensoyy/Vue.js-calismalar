850d327f8afc96adbfbf316b19d71c4d1877b7e7
Nurcan Şensoy <nurcansensoyy@gmail.com>
2022-11-15 11:01:36 +0300

    get push

--- 22-axios-baslangic-kodu/src/components/Posts.vue
+++ 22-axios-baslangic-kodu/src/components/Posts.vue
@@ -3,65 +3,32 @@
     <h1 class="text-center">Yazı Listesi</h1>
     <hr>
     <div class="d-flex flex-wrap flex-row justify-content-center align-items-center">
-      <div class="card m-1">
-        <img class="card-img-top" src="https://i.ytimg.com/vi/a1kDpfwO16s/maxresdefault.jpg" alt="Card image cap">
+      <div v-for = "post in postList" :key="post" class="card m-1">
+        <img class="card-img-top" :src="post.thumbnail" :alt="post.title">
         <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
-            content.</p>
+          <h5 class="card-title">{{post.title}}</h5>
+          <p class="card-text">{{ post.previewText }}</p>
         </div>
       </div>
-      <div class="card m-1">
-        <img class="card-img-top" src="https://i.ytimg.com/vi/a1kDpfwO16s/maxresdefault.jpg" alt="Card image cap">
-        <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
-            content.</p>
-        </div>
-      </div>
-      <div class="card m-1">
-        <img class="card-img-top" src="https://i.ytimg.com/vi/a1kDpfwO16s/maxresdefault.jpg" alt="Card image cap">
-        <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
-            content.</p>
-        </div>
-      </div>
-      <div class="card m-1">
-        <img class="card-img-top" src="https://i.ytimg.com/vi/a1kDpfwO16s/maxresdefault.jpg" alt="Card image cap">
-        <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
-            content.</p>
-        </div>
-      </div>
-      <div class="card m-1">
-        <img class="card-img-top" src="https://i.ytimg.com/vi/a1kDpfwO16s/maxresdefault.jpg" alt="Card image cap">
-        <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
-            content.</p>
-        </div>
-      </div>
-      <div class="card m-1">
-        <img class="card-img-top" src="https://i.ytimg.com/vi/a1kDpfwO16s/maxresdefault.jpg" alt="Card image cap">
-        <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
-            content.</p>
-        </div>
       </div>
     </div>
-  </div>
 </template>
 <script>
 import axios from "axios";
 
 export default {
+  data() {
+    return {
+      postList: []
+    }
+  },
   created() {
-    axios.get("https://vuejs-axios-blog-331e0-default-rtdb.firebaseio.com/posts/-NGqXZ073NEcLh1vGb1p/posts.json")
+    axios.get("https://vuejs-axios-blog-331e0-default-rtdb.firebaseio.com/posts.json")
       .then(response => {
-        console.log(response);
+        let data = response.data;
+        for (let key in data) {
+          this.postList.push({...data[key], id : key})//postList in içine push ladık kartları
+        }
       })
     .catch(e=>console.log(e))
   }
